209e57cba2c9fd552860ae323873316942f95153
Lukasz Grzelczyk <lukasz.grzelczyk@aptiv.com>
2022-12-06 15:38:54 +0100

    day6

--- Day6/solution.py
+++ Day6/solution.py
@@ -1,37 +1,45 @@
-#part 1
 class DataCircBuffer:
     _oldest_el_in_buffer = 0
     _element_count = 0
+    _duplicate_el_idx = 0
 
-    def __int__(self, size:int):
-        self._size=size
-        self.buffer = ['']*size
+    def __init__(self, size: int):
+        self._size = size
+        self.buffer = [''] * size
+        self._duplicate_el_idx = size
 
-    def check_for_char_uniqueness(self, character:str):
-        return True
+    def check_for_char_uniqueness(self, char: str):
+        unique = False
+        for x in range(1, self._size + 1):
+            # start from the newest el in buffer = idx
+            idx = (self._oldest_el_in_buffer - x) % self._size
+            if char == self.buffer[idx]:
+                if self._size - x > self._duplicate_el_idx:
+                    self._duplicate_el_idx = self._size - x
+                break
+        else:
+            if self._duplicate_el_idx <= -1:
+                unique = True
+        self._insert(char)
+
+        return unique
 
     def _insert(self, char):
-        self._element_count +=1
-        self._oldest_el_in_buffer = self._element_count%self._size
+        self.buffer[self._oldest_el_in_buffer] = char
+        self._element_count += 1
+        self._oldest_el_in_buffer = self._element_count % self._size
+        self._duplicate_el_idx -= 1
+
 
 marker_found = False
 with open('input6', mode='r') as f:
-    data_buffer = ['','','']
+    data_buffer = DataCircBuffer(13)
     data_count = 0
-    oldest_el_in_buffer = 0
-    character_duplicate = 3
-    while not marker_found:
-        character = f.read(1)
-        oldest_el_in_buffer = data_count%3
-        duplicate_element_idx = next((x for x in reversed(range(0,len(data_buffer))) if data_buffer[x] == character),-1)
-        if duplicate_element_idx >=0:
-            tmp = (3+duplicate_element_idx - oldest_el_in_buffer)%3
-            if character_duplicate < tmp:
-                character_duplicate = tmp
-        else:
-            character_duplicate -= 1
-        print(f'{data_buffer[oldest_el_in_buffer]}{data_buffer[(oldest_el_in_buffer+1)%3]}{data_buffer[(oldest_el_in_buffer+2)%3]}{character}', character_duplicate)
-        data_buffer[oldest_el_in_buffer] = character
-        data_count +=1
-        if character_duplicate < -1:
+    while character := f.read(1):
+        marker_found = data_buffer.check_for_char_uniqueness(character)
+        data_count += 1
+
+        if marker_found:
+            print("DONE")
+            print(data_count)
             break
